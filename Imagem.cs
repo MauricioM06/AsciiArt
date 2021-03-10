6984b212dd0add556e7f55b2ae5117f9a1ebfd3e
MauricioM06 <78988280+MauricioM06@users.noreply.github.com>
2021-03-10 13:05:21 -0300

    Add files via upload

---
+++ Imagem.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Gato
+{
+
+    public class Program 
+    {
+        public static void Main( String [] args )
+        {
+			Console.WriteLine("  /|_|\\ ");
+            Console.WriteLine(" ( o o )");
+            Console.WriteLine(" ==_Y_== ");
+            Console.WriteLine("   '-'   ");
+        }
+    }
+}
